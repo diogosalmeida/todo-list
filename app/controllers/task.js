8661a668847b1aa09633c8e158975fe2b1726411
diogosalmeida <diogomean@gmail.com>
2017-08-15 13:18:38 -0300

    Criação de função de cadastro de task

--- app/controllers/task.js
+++ app/controllers/task.js
@@ -1,13 +1,17 @@
 const Task = require('../models/task');
 
-module.exports.createTask = () => {
-    console.log(Task);
-}
 
 module.exports.findAll = (req,res) => {
     Task.find((error,tasks) => {
-      if (error) return res.json(error)
-
+        if (error) return res.json(error)
         res.json(tasks);
     });
 }
+
+module.exports.createTask = (task, req, res) => {
+    const taskeInstance = new Task(task);
+    taskeInstance.save((error,task) => {
+      if(error) return res.json(error);
+      res.json(task);
+    });
+ }
