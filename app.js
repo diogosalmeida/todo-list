1c896115e2695093ad7b6bdfa6634a1da3f4fbf6
diogosalmeida <diogomean@gmail.com>
2017-08-15 01:01:18 -0300

    arquivo de configuração do servidor

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const ServerConfig =  require('./config/server')(app);
+
+const port = 4000;
+
+
+app.listen(port, () => {
+    console.log(`Servidor Ligado na porta ${port}`)
+});
