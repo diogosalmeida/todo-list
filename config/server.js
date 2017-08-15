d61c21de1cfa1dbe1e4191076f9518e14e41b805
diogosalmeida <diogomean@gmail.com>
2017-08-15 01:03:41 -0300

    arquivo de configuração do express

---
+++ config/server.js
@@ -0,0 +1,19 @@
+module.exports = (app) => {
+   const bodyParser = require('body-parser');
+   
+   app.use(bodyParser.urlencoded({ extended: false }))
+   
+   app.use(bodyParser.json())
+
+   const consign = require('consign');
+   
+   consign({cwd:'app'})
+    .include('models')
+    .then('controllers')
+    .into(app);
+
+    //Rotas 
+        const indexRouter = require('../app/routes/index')(app)
+        const tasksRouter = require('../app/routes/task')(app)
+    //
+}
