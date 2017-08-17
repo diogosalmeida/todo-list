96e23940bb3dde62a2dda2a3c23e707562dbc775
diogosalmeida <diogomean@gmail.com>
2017-08-17 17:15:24 -0300

    Correção de identação

--- app/controllers/task.js
+++ app/controllers/task.js
@@ -7,7 +7,6 @@ module.exports.findAll = (req, res) => {
         res.json(tasks);
     });
 }
-
 module.exports.createTask = (task, req, res) => {
     const taskeInstance = new Task(task);
     taskeInstance.save((error, task) => {
@@ -15,15 +14,20 @@ module.exports.createTask = (task, req, res) => {
         res.json(task);
     });
 }
-
 module.exports.updateTask = (req, res, id, taskUpdate) => {
-
     let query = Task.where({ _id: id }).update({
         title: req.body.title,
         author: req.body.author,
         bodyTask: req.body.bodyTask,
         bodyComment: req.body.bodyComment
     },(error, task)=>{
+        if(error) res.json(error);
+        res.json(task)
+    });
+}
+module.exports.findTask = (req,res, id) => {
+    let query = Task.where({ _id: id });
+    query.findOne((error, task)=>{
         if(error) res.json(error);
         res.json(task)
     });
